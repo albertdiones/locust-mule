8f4cce14ea64b61e2a0e4867f4cd002bedb5cbd0
Albert Diones <albertdiones@gmail.com>
2016-01-18 20:04:11 +0800

    Use the config

--- www/includes/classes/locust_mule_db.class.php
+++ www/includes/classes/locust_mule_db.class.php
@@ -20,6 +20,6 @@ CLASS locust_mule_db EXTENDS add_adodb_mysql {
      * 
      */
     public function Connect() {
-        return $this->adodb->Connect("localhost","root","","mydb");
+        return $this->adodb->Connect( add::config()->mysql_host, add::config()->mysql_username,add::config()->mysql_password, add::config()->mysql_db_name );
     }
 }
