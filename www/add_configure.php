654ce87457a1773ba72a4908e81fbadd7af6e3a3
Albert Diones <albertdiones@gmail.com>
2016-01-18 22:51:58 +0800

    Make way for framework based setting of default timezone

--- www/add_configure.php
+++ www/add_configure.php
@@ -3,4 +3,4 @@ require 'config.php';
 #echo "ADD DIR: ".$C->add_dir;
 require $C->add_dir.'/init.php';
 
-date_default_timezone_set(add::config()->default_timezone);
+#date_default_timezone_set(add::config()->default_timezone);
