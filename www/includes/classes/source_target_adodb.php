22d2e4dc27c57c70a9093b460266448c402f95b8
Albert Diones <albertdiones@gmail.com>
2016-01-18 23:11:10 +0800

    Fake meta_quote for compatibility

--- www/includes/classes/source_target_adodb.php
+++ www/includes/classes/source_target_adodb.php
@@ -12,4 +12,8 @@ CLASS source_target_adodb EXTENDS add_adodb {
    public function Connect() {
       return call_user_func_array(array($this->adodb,'Connect'),func_get_args());
    }
+
+   public function meta_quote($string) {
+      return $string;
+   }
 }
