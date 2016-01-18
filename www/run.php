30a22ab49e06c3879162a94aab117d75e1ccd18f
Albert Diones <albertdiones@gmail.com>
2016-01-18 23:09:46 +0800

    bug fixed: source_table_pk instead of source_pk bug fixed: $target instead of $project

--- www/run.php
+++ www/run.php
@@ -19,7 +19,7 @@ debug::log("running project: " . $project);
 # 2. Checks the source table for Rows Per Run number of rows that is greater than Last Id (ordered by id ASC)
 $source_db = new source_target_adodb(ADONewConnection($project->source_db_type));
 $source_db->Connect($project->source_host, $project->source_username, $project->source_password);
-$source_model = (string) new fluid_model($project->source_table,$project->source_table_pk,$source_db);
+$source_model = (string) new fluid_model($project->source_table,$project->source_pk,$source_db);
 $source_data = $source_model::get_where_order_page(
    $source_model::table_pk().' > '. $source_model::db()->quote($project->last_id),
    $source_model::table_pk().' ASC',
@@ -32,7 +32,7 @@ debug::log("Fetched rows from source - count: " . count($source_data));
 # 4. Inserts the rows to target table
 $target_db = new source_target_adodb(ADONewConnection($project->target_db_type));
 $target_db->Connect($project->target_host, $project->target_username, $project->target_password);
-$target_model = (string) new fluid_model($target->target_table,$target->target_table_pk,$target_db);
+$target_model = (string) new fluid_model($project->target_table,$project->target_pk,$target_db);
 foreach ($source_data as $source_datum) {
    $target_model::add_new($source_datum->data_array());
    debug::log("Inserted row to target: " . $source_datum);
