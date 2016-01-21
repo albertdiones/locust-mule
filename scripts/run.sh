be46c99b10692ff6c392a322377d1c35c459bb7e
Albert Diones <albertdiones@gmail.com>
2016-01-21 23:45:36 +0800

    Bug Fix: Memory only checked once

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,11 @@
 # Run continously, put this in crontab with flock
 # e.g. * * * * * flock -n /var/run/locust-mule-run.lockfile -c /var/git/locust-mule:master/scripts/run.sh
 
-FREE_MEMORY=$(grep "Inactive:" /proc/meminfo |  awk '{print $2}')
-TOTAL_MEMORY=$(grep MemTotal /proc/meminfo |  awk '{print $2}')
-ALLOWANCE_MEMORY=$(printf "%.0f" $(echo "$TOTAL_MEMORY * 0.02" | bc))
-
 while [ True ]
 do
+   FREE_MEMORY=$( echo $(grep "MemFree:" /proc/meminfo |  awk '{print $2}') + $(grep "Inactive:" /proc/meminfo |  awk '{print $2}') | bc )
+   TOTAL_MEMORY=$(grep MemTotal /proc/meminfo |  awk '{print $2}')
+   ALLOWANCE_MEMORY=$(printf "%.0f" $(echo "$TOTAL_MEMORY * 0.05" | bc))
    if [ $FREE_MEMORY -gt $ALLOWANCE_MEMORY ]
    then
       php -f ../www/run.php
